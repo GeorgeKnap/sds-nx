6f57f070a1753527a41f631a2dd84a5a54cfcfa5
George Knap <jirka.knap@gmail.com>
2020-03-01 12:44:28 +0100

    refactor: 💡 testing dependency

--- apps/enerest-web4/src/app/app.module.ts
+++ apps/enerest-web4/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ApiModule } from "@sds/api";
+import { ApiModule } from '@sds/api';
 import { AppComponent } from './app.component';
 
 
